eae23ed1033365d7c29de196afa5d82b75a0d079
SyahmiManaf <syahmimnf26@gmail.com>
2026-04-12 15:12:24 +0800

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Analytics />
         <Toaster position="top-center" richColors />
       </QueryClientProvider>
     </BrowserRouter>
